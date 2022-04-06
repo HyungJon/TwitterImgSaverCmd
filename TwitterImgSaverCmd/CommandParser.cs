35793efdbb756c0f04ebb51e077db57e11bf51f2
Hyung Jon Kim <hyungjon.kim@gmail.com>
2022-04-06 18:16:02 +0800

    Improved parse logic

--- TwitterImgSaverCmd/CommandParser.cs
+++ TwitterImgSaverCmd/CommandParser.cs
@@ -17,32 +17,32 @@ namespace TwitterImgSaverCmd
     {
         public static ICommand ParseCommand(string input, IConfiguration configs)
         {
-            var parameters = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var (commandType, parameters) = GetCommandTypeAndParams(input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
 
-            // below logic could be improved
-            // but I also want to avoid having to check the first element multiple times
-            // once to check if it specified command, second if true to check the type of command
+            if (!parameters.Any()) throw new InvalidOperationException("Unexpected failure: no parameter entered");
 
-            // handle some special cases
-            if (DetectSingleAddressCase(parameters))
-                return new DownloadCommand(input, configs);
-
-            if (DetectChdirCase(parameters))
-                return new ChdirCommand(parameters[1].Trim(), configs);
-
-            var downloadersCandidate = HandleDownloadWithCommandCase(parameters, configs);
-            if (downloadersCandidate != null) return downloadersCandidate;
-
-            // barring further complications in logic, anything beyond this can be treated as request to download from all given parameters
-            downloadersCandidate = HandleMultipleDownloadsWithoutCommandCase(parameters, configs);
-            if (downloadersCandidate != null) return downloadersCandidate;
+            switch (commandType)
+            {
+                case null:
+                    return new AggregateCommand(parameters.Select(p => new DownloadCommand(p, configs)), configs);
+                case CommandType.ChangeDir:
+                    if (parameters.Count > 2)
+                        throw new InvalidOperationException("Could not parse input: incorrect number of parameters");
+                        // actually, could consider saving to multiple locations, using add/remove/clear directory command instead
+                        // also could just ignore the rest 
+                    else
+                        return new ChdirCommand(parameters[0], configs);
+                case CommandType.Download:
+                    return new AggregateCommand(parameters.Select(p => new DownloadCommand(p, configs)), configs);
+            }
 
             throw new InvalidOperationException("Could not parse input: unrecognized command");
         }
 
-        private static (CommandType, IList<string>) GetCommandTypeAndParams(string[] parameters)
+        private static (CommandType?, IList<string>) GetCommandTypeAndParams(string[] parameters)
         {
-            // may need update to below logic
+            // this logic was implemented in case the parsing logic is updated so that there can be input without explicit command that isn't download
+            // in which case the method that receives this should need to further parse the input to figure out what the command is
             switch (parameters[0])
             {
                 case "chdir":
@@ -50,35 +50,8 @@ namespace TwitterImgSaverCmd
                 case "download":
                     return (CommandType.Download, parameters.Skip(1).ToList());
                 default:
-                    return (CommandType.ChangeDir, parameters.ToList());
+                    return (null, parameters.ToList());
             }
         }
-
-        private static bool DetectSingleAddressCase(string[] parameters)
-        {
-            // return parameters.Length == 1 && Uri.TryCreate(parameters[0], UriKind.Absolute, out Uri _);
-            return parameters.Length == 1; // for now, no other use case where just one parameter is supplied
-        }
-
-        private static bool DetectChdirCase(string[] parameters)
-        {
-            return parameters.Length == 2 && parameters[0] == "chdir";
-        }
-
-        private static ICommand HandleDownloadWithCommandCase(string[] parameters, IConfiguration configs)
-        {
-            if (parameters[0] == "download")
-            {
-                if (parameters.Length == 2)
-                    return new DownloadCommand(parameters[1].Trim(), configs);
-                else
-                    return new AggregateCommand(parameters.Skip(1).Select(p => new DownloadCommand(p, configs)), configs);
-            }
-
-            return null;
-        }
-
-        private static ICommand HandleMultipleDownloadsWithoutCommandCase(string[] parameters, IConfiguration configs)
-            => new AggregateCommand(parameters.Select(p => new DownloadCommand(p, configs)), configs);
     }
 }
