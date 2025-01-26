cdca030f56227e218ac445afd323fd3a941da156
Kim Hyung Jon <hyungjon.kim@gmail.com>
2025-01-26 10:39:11 +0800

    Updated parser to have proper handling of implicit command and following parameters and separated the parsing logic for different command type (implicit or explicit)

--- CommandParser.cs
+++ CommandParser.cs
@@ -28,28 +28,27 @@ public static class CommandParser
         var (commandType, parameters) = GetCommandTypeAndParams(arguments);
 
         if (!parameters.Any()) throw new InvalidOperationException("Unexpected failure: no parameter entered");
-
+        
         switch (commandType)
         {
-            case null:
-                return ParseNonExplicitInput(parameters, configs);
             case CommandType.ChangeDir:
                 if (parameters.Count > 2)
                     throw new InvalidOperationException("Could not parse input: incorrect number of parameters");
                 // TODO: once saving to multiple directories is implemented, remove this restriction
                 return new ChdirCommand(parameters[0], configs);
-            case CommandType.Download:
-                return new AggregateCommand(parameters.Distinct().Select(p => new DownloadCommand(p, configs)), configs);
             case CommandType.AddShortcut:
                 if (parameters.Count != 2)
                     throw new InvalidOperationException("Could not parse input: incorrect number of parameters");
                 return new AddShortcutCommand(parameters[0].ToLower(), parameters[1], configs);
+            case CommandType.Download:
+                return ProcessDownloadCommand(parameters, configs);
+                // return new AggregateCommand(parameters.Distinct().Select(p => new DownloadCommand(p, configs)), configs);
+            default:
+                throw new InvalidOperationException("Could not parse input: unrecognized command");
         }
-
-        throw new InvalidOperationException("Could not parse input: unrecognized command");
     }
 
-    private static (CommandType?, IList<string>) GetCommandTypeAndParams(string[] parameters)
+    private static (CommandType, IList<string>) GetCommandTypeAndParams(string[] parameters)
     {
         // this logic was implemented in case the parsing logic is updated so that there can be input without explicit command that isn't download
         // in which case the method that receives this should need to further parse the input to figure out what the command is
@@ -58,48 +57,50 @@ public static class CommandParser
             "chdir" => (CommandType.ChangeDir, parameters.Skip(1).ToList()),
             "download" => (CommandType.Download, parameters.Skip(1).ToList()),
             "addshortcut" => (CommandType.AddShortcut, parameters.Skip(1).ToList()),
-            _ => (null, parameters.ToList())
+            _ => (InferCommandType(parameters.ToList()), parameters.ToList())
         };
     }
 
-    private static ICommand ParseNonExplicitInput(IList<string> parameters, IConfiguration configs)
+    private static CommandType InferCommandType(IList<string> parameters)
     {
-        // temp code: as commented above, this may need to be updated in the future if more commands are allowed to be non-explicit
-        // C# can distinguish between web link and file path using new Uri(path).IsFile, consider enabling non-explicit chdir
-        
-        // TODO: due to this being the most-used input format, most functionality is revolving around this
-        // TODO: simplifying the parsing has become a tech debt
+        return CommandType.Download; // Temporary code: just assume download
+    }
+
+    private static ICommand ProcessDownloadCommand(IList<string> parameters, IConfiguration configs)
+    {
+        string? filenameOverride = null;
 
         if (!Uri.TryCreate(parameters[0], UriKind.Absolute, out _))
         {
-            var filename = parameters[0];
-
-            var (savePath, imageLinks) = ParseSourcesAndShortcut(parameters.Skip(1).ToList(), configs);
-                
-            if (!imageLinks.Any())
-            {
-                throw new InvalidOperationException("No links to save provided");
-            }
-
-            if (imageLinks.Count == 1)
-            {
-                return new DownloadCommand(imageLinks[0], configs, filename, savePath);
-            }
-
-            var downloadCommands = new List<DownloadCommand>();
-            for (var i = 0; i < imageLinks.Count; i++)
-            {
-                // note: in the old implementation where the tweet attachments list could be directly derived from a tweet metadata,
-                // the index assignment was done in TweetImagesDownloader
-                // this is inevitable now that the old method cannot be used any more, but it's possible that moving index assignment logic
-                // further down (e.g. create a new Command subclass) may be preferable
-                downloadCommands.Add(new DownloadCommand(imageLinks[i], configs, filename + '_' + (i + 1), savePath));
-            }
-            return new AggregateCommand(downloadCommands, configs);
-
+            filenameOverride = parameters[0];
+            parameters.RemoveAt(0);
         }
 
-        return new AggregateCommand(parameters.Distinct().Select(p => new DownloadCommand(p, configs)), configs);
+        if (configs.SavePathShortcuts.TryGetValue(parameters.Last(), out var savePathOverride))
+        {
+            parameters.RemoveAt(parameters.Count - 1);
+        }
+        
+        if (!parameters.Any())
+        {
+            throw new InvalidOperationException("No links to save provided");
+        }
+
+        if (parameters.Count == 1)
+        {
+            return new DownloadCommand(parameters[0], configs, filenameOverride, savePathOverride);
+        }
+
+        var downloadCommands = new List<DownloadCommand>();
+        for (var i = 0; i < parameters.Count; i++)
+        {
+            // note: in the old implementation where the tweet attachments list could be directly derived from a tweet metadata,
+            // the index assignment was done in TweetImagesDownloader
+            // this is inevitable now that the old method cannot be used any more, but it's possible that moving index assignment logic
+            // further down (e.g. create a new Command subclass) may be preferable
+            downloadCommands.Add(new DownloadCommand(parameters[i], configs, filenameOverride + '_' + (i + 1), savePathOverride));
+        }
+        return new AggregateCommand(downloadCommands, configs);
     }
 
     private static (string?, IList<string>) ParseSourcesAndShortcut(IList<string> parameters, IConfiguration configs)
